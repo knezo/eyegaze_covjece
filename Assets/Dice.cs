6a30d0bf35744d971f9b1d08fd1a0bfc43748c1d
kknezic <knezic.kristijan@gmail.com>
2020-11-09 23:20:15 +0100

    initial commit

---
+++ Assets/Dice.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+
+    public Sprite[] diceSides;
+    public SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        diceSides = Resources.LoadAll<Sprite>("Dice");
+        rend.sprite = diceSides[5];
+    }
+
+    void OnMouseDown()
+    {
+        StartCoroutine("RollTheDice");
+    }
+
+    IEnumerator RollTheDice()
+    {
+        int randomDiceSide = 0;
+        for (int i = 0; i < 12; i++)
+        {
+            randomDiceSide = Random.Range(0,6);
+            rend.sprite = diceSides[randomDiceSide];
+            yield return new WaitForSeconds(0.1f);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
